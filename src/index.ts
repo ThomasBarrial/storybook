b25f652df9defe2e0f58582eef7721174d71bdaf
Thomas Barrial <thomas.barrial@gmail.com>
2022-03-11 12:49:31 +0100

    test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import Button from './components/buttons/button/Button';
 import ButtonTailwind from './components/buttons/buttonTailwind/ButtonTailwind';
-import SlideUp from './components/animation/slideUp/SlideUp';
+
 import FadeInButton from './components/buttons/fadeInButton/FadeInButton';
 
-export { Button, ButtonTailwind, SlideUp, FadeInButton };
+export { Button, ButtonTailwind, FadeInButton };
