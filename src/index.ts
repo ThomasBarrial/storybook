0cd9db5fb7f85c34b823c6ab13c1fae762950081
Thomas Barrial <thomas.barrial@gmail.com>
2022-03-11 12:55:23 +0100

    delete bug file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import Button from './components/buttons/button/Button';
 import ButtonTailwind from './components/buttons/buttonTailwind/ButtonTailwind';
-
 import FadeInButton from './components/buttons/fadeInButton/FadeInButton';
 
 export { Button, ButtonTailwind, FadeInButton };
